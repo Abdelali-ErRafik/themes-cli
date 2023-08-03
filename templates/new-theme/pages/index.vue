606271fea84c73b0cab7328c5327b04e17434ae2
Abdelali-ErRafik <abdelali.rafik11@gmail.com>
2023-08-03 15:00:59 +0100

    add new themes

---
+++ templates/new-theme/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- you can delete this StoreinoWelcome component -->
+    <storeino-welcome></storeino-welcome>
+    <!-- Components Examples To Start Coding -->
+    <!-- <sections-settings></sections-settings> -->
+    <!-- <sections-fetch></sections-fetch> -->
+    <!-- <sections-application></sections-application> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
